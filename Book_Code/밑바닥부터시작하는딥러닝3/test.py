4018dc3825bb68758cf4a157d80da319fa338c4d
johwiyong <airplane2230@naver.com>
2021-04-06 22:41:07 +0900

    0406

---
+++ Book_Code/밑바닥부터시작하는딥러닝3/test.py
@@ -0,0 +1,19 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import _dot_var
+
+def sphere(x, y):
+    z = x ** 2 + y ** 2
+    return z
+
+def matyas(x, y):
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
+
+x = Variable(np.random.randn(2, 3))
+x.name = 'x'
+print(_dot_var(x))
