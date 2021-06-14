70733ae6a6c5c315a43d2e810cc55c6899cb8e08
johwiyong <airplane2230@naver.com>
2021-06-14 22:43:13 +0900

    0614

--- Book_Code/밑바닥부터시작하는딥러닝3/test.py
+++ Book_Code/밑바닥부터시작하는딥러닝3/test.py
@@ -1,19 +1,13 @@
-if '__file__' in globals():
-    import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import numpy as np
 from dezero import Variable
-from dezero.utils import _dot_var
+import dezero.functions as F
+import numpy as np
 
-def sphere(x, y):
-    z = x ** 2 + y ** 2
-    return z
 
-def matyas(x, y):
-    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
-    return z
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = 5 + 2 * x + np.random.rand(100, 1)
+x, y = Variable(x), Variable(y)
 
-x = Variable(np.random.randn(2, 3))
-x.name = 'x'
-print(_dot_var(x))
+W = Variable(np.zeros((1, 1)))
+b = Variable(np.zeros(1))
